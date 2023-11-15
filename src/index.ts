1deeadab918ad02d248ad56ddbe0aab98f191dbc
Matias Risco <matiasriscolagos@gmail.com>
2023-11-15 12:15:27 +0100

    Add ping router

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import morgan from "morgan";
+import express from "express";
 import { notFound } from "./middleware/error/errorMiddleware.js";
 import thingsRouter from "./features/things/router/thingsRouter.js";
 import app from "./app.js";
+import pingRouter from "./features/ping/router/pingRouter.js";
 
+app.use(express.json());
 app.use(morgan("dev"));
-
+app.use("/", pingRouter);
 app.use("/things", thingsRouter);
 
 app.use(notFound);
